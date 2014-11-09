bd9f15dc76ed95bf8ece0cf8ca3edcac0cb41773
Celso Junior <celsojr100@gmail.com>
2014-11-09 15:49:19 -0200

    Some bugs were killed and now everything is working as expected

--- py_short_url/shortener/converter.py
+++ py_short_url/shortener/converter.py
@@ -1,12 +1,15 @@
 # Dictionary of Base62 (position is the key and chars, the value)
 base = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-base_value = 62
+base_value = len(base)
 
 # Function that converts any string (supposed to be a base62 number) to a correspondent decimal number
 def toDecimal(string):
     sum = 0
-    for exp in range(len(string)):
-        sum = sum + pow(62,exp)*base.index(string[exp])
+    position = 0
+    # Generate a downto for to calculate correspodent decimal value to a string
+    for exp in range(len(string),-1,-1)[1:]:
+        sum = sum + pow(base_value,exp)*base.index(string[position])
+        position = position + 1
 
     return sum
 
@@ -19,13 +22,13 @@ def toString(number):
     final_list.append(base[remainder])
 
     # Uses 'serie of divisions' method to convert any number to a base62 string
-    while (number >= base_value):
+    while (number > base_value):
         remainder = number % base_value
         number = number / base_value
         final_list.append(base[remainder])
 
     final_list.append(base[number])
-    final_list
+    final_list.reverse()
 
     # Converts a list to a string
     final_string = ''.join(final_list)
